419544a32e5e1d77d10d12df26c07d085b86c45a
Logan Campbell <colin@logaan.net>
2019-10-28 21:15:35 +1100

    dropped comments, piping in run

--- lisp.go
+++ lisp.go
@@ -12,22 +12,20 @@ func main() {
 }
 
 func tokenise(program string) []string {
-	// replace ( and ) with " ( " and " ) "
 	spaced := regexp.MustCompile("[()]").ReplaceAllString(program, " $0 ")
-	// split on \w*, return all substrings
 	return regexp.MustCompile("[ \n]+").Split(spaced, -1)
 }
 
 func readStdin() string {
 	result := ""
-	fmt.Println("Echoing")
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for scanner.Scan() {
 		result = result + scanner.Text() + "\n"
 	}
 
 	if scanner.Err() != nil {
-		fmt.Println("Some error occurred.")
+		fmt.Println("Failed to read from stdin.")
 	}
 
 	return result
